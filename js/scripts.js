e9ccb7969635ea85d9cc18d65344735128bf3224
hopekayumov <you@example.com>
2021-05-05 04:30:18 +0300

    all

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$('.universities__list').slick({
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    dots: true,
+    arrow: false,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    infinite: true,
+    pauseOnHover: false
+
+});
